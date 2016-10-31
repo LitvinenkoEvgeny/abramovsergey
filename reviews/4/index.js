9679c9dd90b14ca80074fd4ea812434e030f6299
LitvinenkoEvgeny <yourbisnesspartner@gmail.com>
2016-10-31 19:59:10 +0300

    start write rewiews files

--- reviews/4/index.js
+++ reviews/4/index.js
@@ -4,21 +4,17 @@ $("#show-review").removeClass('hidden').children('.handle').html([
   '<div class="pop-up review">',
      '<h3>Благодарность</h3>',
     	'<div class="authors">',
-          // ССЫЛКА НА СТРАНИЦЫ АВТОРОВ
     			'<a target="_blank" rel="nofollow" href="https://vk.com/id32002161" class="groom"><img alt="4yzc03r7pfe" src="https://pp.vk.me/c604326/v604326161/96be/4yzC03r7PFE.jpg">Роман </a>',
     			'<div class="amp">&amp;</div>',
     			'<a target="_blank" rel="nofollow" href="https://vk.com/irishkinkin" class="bride"><img alt="Dwv2pnfa7pq" src="https://pp.vk.me/c636531/v636531762/201bd/dwv2pNfa7pQ.jpg">Ирина</a>',
     		'<div class="line"></div>',
     	'</div>',
   	'<div id="show-review-nav">',
-      // ТУТ УКАЗЫВАТЬ ССЫЛКУ ДЛЯ ПЕРЕХОДА НА ПРЕДЫДУЩИЙ/СЛЕДУЮЩИЙ ОТЗЫВ
-  		'<a class="prev" data-remote="true" href="reviews/105"></a>',
-  		'<a class="next" data-remote="true" href="reviews/2/index.js"></a>',
+  		'<a class="prev" data-remote="true" href="reviews/3/index.js"></a>',
+  		'<a class="next" data-remote="true" href="reviews/5/index.js"></a>',
   	'</div>',
-    // МЕНЯТЬ ТУТ ССЫЛКУ НА БОЛЬШУЮ ФОТКУ
-		'<img alt="" class="photo" src="reviews/1/1.jpg">',
-    // ТЕКСТ ОТЗЫВА
+		'<img alt="" class="photo" src="reviews/4/4.jpg">',
   	'<p class="text">Любой текст</p>',
-  	'<p class="date"><img src="date.svg">06 сентября 2015</p>',
+  	'<p class="date"><img src="date.svg">10 октября 2015</p>',
   '</div>'
 ].join('\n'));
